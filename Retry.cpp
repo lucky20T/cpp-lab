b8a79b489512c7c5caef8ae99d5e79b92046d671
Vishal Chakradhari <vishalchakradhari2005@gmail.com>
2025-10-28 21:54:43 +0530

    Initial commit

--- Retry.cpp
+++ Retry.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
 // using namespace std;
 
-int main() {
-    std::cout << "Hello, World!\n" ;
+int main(int argc, char* argv[]) {
+    std::vector<int> vec;
+    vec.push_back(20);
+    vec.push_back(29);
+    vec.push_back(30);
+    for (size_t i = 0; i < vec.size();++i)
+    {
+        std::cout << vec[i] <<"\n";
+    }
     return 0;
 }
