72df54664e8177e60b3da7ef9c609fffd6879f0b
Vishal Chakradhari <vishalchakradhari2005@gmail.com>
2025-10-29 17:36:26 +0530

    no point

--- Retry.cpp
+++ Retry.cpp
@@ -1,30 +1,65 @@
 #include <iostream>
 #include <vector>
 // using namespace std;
+class Student
+{   std::string m_first = "first";
+    std::string m_last  = "last";
+    int         m_age   = 0;
+    int         m_id    = 0;
 
-int main(int argc, char* argv[]) {
-    std::vector<int> vec;
-    vec.push_back(20);
-    vec.push_back(29);
-    vec.push_back(30);
-    for (size_t i = 0; i < vec.size();++i)
+
+    public:
+
+    Student(std::string first, std::string last, int age, int id)
+        :m_first(first),
+         m_last(last), 
+         m_age(age), 
+         m_id(id)
     {
-        std::cout << vec[i] <<"\n";
+
     }
-    std::cout << "----\n";
-    for (auto it = vec.begin(); it != vec.end(); ++it)
+
+    int getAge()
     {
-        std::cout << *it << "\n";   
+        return m_age;
     }
-    std::cout << "----\n";
-    for (int val : vec)
+    std::string getFirst()
     {
-        std::cout << val << "\n";   
+        return m_first;
     }
-    std::cout << "----\n";
-    for(auto val:vec)
+    std::string getLast()
     {
-        std::cout << val << "\n";   
+        return m_last;
     }
-    return 0;
-}
+    int getId()
+    {
+        return m_id;
+    }
+};
+
+// int main(int argc, char* argv[]) {
+//     std::vector<int> vec;
+//     vec.push_back(20);
+//     vec.push_back(29);
+//     vec.push_back(30);
+//     for (size_t i = 0; i < vec.size();++i)
+//     {
+//         std::cout << vec[i] <<"\n";
+//     }
+//     std::cout << "----\n";
+//     for (auto it = vec.begin(); it != vec.end(); ++it)
+//     {
+//         std::cout << *it << "\n";   
+//     }
+//     std::cout << "----\n";
+//     for (int val : vec)
+//     {
+//         std::cout << val << "\n";   
+//     }
+//     std::cout << "----\n";
+//     for(auto val:vec)
+//     {
+//         std::cout << val << "\n";   
+//     }
+//     return 0;
+// }
