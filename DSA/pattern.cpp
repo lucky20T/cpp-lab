040a8fc48df54952c4a49c06759562b4ee43b39f
Vishal Chakradhari <vishalchakradhari2005@gmail.com>
2026-03-09 22:36:53 +0530

    pattrenlefthollodiamido

---
+++ DSA/pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter any number:";
+    cin>>n;
+
+    int num=1;
+
+    for(int i = 0;i<n;i++){
+        for(int j=0; j<n; j++){
+            cout<<num <<" ";
+            num++;
+        }
+        cout<<"\n";
+    }
+    cout<<"Pattern of "<<n<<"*"<<n<<"\n";
+    cout<<"Total Number:"<<num-1; 
+}
